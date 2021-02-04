add142f8e199ae3ba6cd028a25c7be7c9e41604c
jungsunhwan <znzn9292@gmail.com>
2021-02-05 00:19:09 +0900

    Init

---
+++ src/main/java/com/znzn/sample/nonstop/NonstopApplication.java
@@ -0,0 +1,13 @@
+package com.znzn.sample.nonstop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonstopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NonstopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/znzn/sample/nonstop/NonstopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.znzn.sample.nonstop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonstopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
